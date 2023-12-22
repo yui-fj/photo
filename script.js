fc0274d68df334313e2b73037e2b9b0c9c357fad
yui-fj <145320025+yui-fj@users.noreply.github.com>
2023-12-22 11:25:03 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    autoplay:true,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+  // JavaScript
+
+const items = document.querySelectorAll('.img-item');
+
+// console.log(items);
+// console.log(items.length);
+
+// for文
+for(let i = 0; i < items.length; i++){
+
+  const keyframes = {
+    opacity: [0, 1],
+    translate: ['0 50px', 0],
+    filter: ['blur(10px)', 'blur(0)'],
+  };
+  
+  const options = {
+    duration: 600,
+    delay: i * 400,
+    fill: 'forwards',
+  };
+
+  items[i].animate(keyframes, options);
+}
+
+// forEach文
+// items.forEach((item, i) => {
+
+//   const keyframes = {
+//     opacity: [0, 1],
+//     translate: ['0 50px', 0],
+//     filter: ['blur(10px)', 'blur(0)'],
+//   };
+  
+//   const options = {
+//     duration: 600,
+//     delay: i * 400,
+//     fill: 'forwards',
+//   };
+
+//   item.animate(keyframes, options);
+// });
